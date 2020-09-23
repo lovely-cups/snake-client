7a2396e0b3bc1b4da68ea86336a8e745d4df6a62
‘Charles <charlesthompson.2693@gmail.com>
2020-09-23 23:08:50 +0000

    added keyboard functions

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -7,13 +10,26 @@ const setupInput = function() {
     handleUserInput(key);
   });
   return stdin;
-}
+};
 
 const handleUserInput = (key) => {
   const stdout = process.stdout;
-  if (key === '\u0003') 
+  if (key === '\u0003') {
+    stdout.write("Buh-Bye");
     process.exit();
 }
+if(key === 'w'){
+  connection.write("Move: Up");
+}
+if(key === "a"){
+  connection.write("Move: Left");
+}
+if(key === "s"){
+  connection.write("Move: Down");
+}
+if(key === "d"){
+  connnection.write("Move: Right");
+  }
+};
 
-module.exports = setupInput;
-module.exports = handleUserInput;
+module.exports = { setupInput };
